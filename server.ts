c8723dc9517fb92dcb756d774ac53322268f297f
Norbek Qodirov <norbekqodirov@gmail.com>
2026-04-25 02:16:38 +0500

    fix: ReferenceError __dirname is not defined in ES modules environment

--- server.ts
+++ server.ts
@@ -277,9 +277,9 @@ async function startServer() {
     });
     app.use(vite.middlewares);
   } else {
-    app.use(express.static('dist'));
+    app.use(express.static(process.cwd() + '/dist'));
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+      res.sendFile(path.resolve(process.cwd(), 'dist', 'index.html'));
     });
   }
 
